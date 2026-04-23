15a92b7b2a5fca7eee3fea1225c8fc3d04908b56
IT25103737 <it25103737@my.sliit.lk>
2026-04-23 22:56:18 +0530

    fix error

--- src/main/java/com/bikerental/BikeRentalSystem/model/CashPayment.java
+++ src/main/java/com/bikerental/BikeRentalSystem/model/CashPayment.java
@@ -1,4 +1,12 @@
 package com.bikerental.BikeRentalSystem.model;
 
 public class CashPayment {
+    class CardPayment extends Payment {
+        private String cardNumber;
+        private String cardHolderName;
+        private String expiryDate;
+        
+
+
+
 }
