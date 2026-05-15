d89a8b8c091f74a2791e2f1abf73819d37390811
DIMANTHA2003054 <it25103738@my.sliit.lk>
2026-05-15 09:11:38 +0530

    Update Station.java
    add coments

--- src/main/java/com/bikerental/BikeRentalSystem/model/Station.java
+++ src/main/java/com/bikerental/BikeRentalSystem/model/Station.java
@@ -1,15 +1,19 @@
 package com.bikerental.BikeRentalSystem.model;
 
+// 'abstract' means you can't create a "Station" object directly. 
+// You must create a 'MainStation' or 'SatelliteStation' instead.
 public abstract class Station {
 
-    private String id;
-    private String name;
-    private String address;
-    private String city;
-    private int capacity;
-    private int bikeCount;
-    private String type;
+    // These are the details (data) every station must have
+    private String id;       // Unique ID for the station
+    private String name;     // Name of the station
+    private String address;  // Street address
+    private String city;     // City location
+    private int capacity;    // Maximum bikes the station can hold
+    private int bikeCount;   // Number of bikes currently at the station
+    private String type;     // Type of station (Main or Satellite)
 
+    // Constructor: This sets up all the information when a new station is created
     public Station(String id, String name, String address, String city, int capacity, int bikeCount, String type) {
         this.id = id;
         this.name = name;
@@ -20,29 +24,41 @@ public abstract class Station {
         this.type = type;
     }
 
+    // This is a "rule." Every class that extends Station MUST provide its own version of this method.
     public abstract String getStationType();
 
+    // A simple check to see if there is room for more bikes
     public boolean hasSpace() {
-        return bikeCount < capacity;
+        return bikeCount < capacity; // Returns true if current bikes are less than total capacity
     }
 
-    // Getters and setters
+    // Getters and Setters: These are used to safely read or update the private data above
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
+    
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    
     public String getAddress() { return address; }
     public void setAddress(String address) { this.address = address; }
+    
     public String getCity() { return city; }
     public void setCity(String city) { this.city = city; }
+    
     public int getCapacity() { return capacity; }
     public void setCapacity(int capacity) { this.capacity = capacity; }
+    
     public int getBikeCount() { return bikeCount; }
     public void setBikeCount(int bikeCount) { this.bikeCount = bikeCount; }
+    
     public String getType() { return type; }
     public void setType(String type) { this.type = type; }
 
+    // This converts all the station info into a single line of text separated by "|"
+    // Useful for saving the data into a text file.
     public String toFileString() {
         return id + "|" + name + "|" + address + "|" + city + "|" + capacity + "|" + bikeCount + "|" + type;
+    }
+}dress + "|" + city + "|" + capacity + "|" + bikeCount + "|" + type;
     }
 }
