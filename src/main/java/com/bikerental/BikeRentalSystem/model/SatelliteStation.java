a706f3d71b510f9b7eef89aca8ec8a548ab7fc5e
DIMANTHA2003054 <it25103738@my.sliit.lk>
2026-05-15 09:07:54 +0530

    Update SatelliteStation.java
    add comments and change line number 9 and 13

--- src/main/java/com/bikerental/BikeRentalSystem/model/SatelliteStation.java
+++ src/main/java/com/bikerental/BikeRentalSystem/model/SatelliteStation.java
@@ -1,13 +1,19 @@
-package com.bikerental.BikeRentalSystem.model;
+package com.bikerental.BikeRentalSystem.model; // The folder where this file lives
 
-import com.bikerental.BikeRentalSystem.util.AppConstants;
+import com.bikerental.BikeRentalSystem.util.AppConstants; // Importing shortcut names for station types
 
+// This class is a "SatelliteStation," which is a special type of "Station"
 public class SatelliteStation extends Station {
 
+    // This is the setup (Constructor) for creating a new Satellite Station
     public SatelliteStation(String id, String name, String address, String city, int capacity, int bikeCount) {
+        
+        // This sends all the info to the main "Station" class to save it
+        // It also labels this station as a 'SAT' (Satellite) type
         super(id, name, address, city, capacity, bikeCount, AppConstants.STATION_SAT);
     }
 
+    // This method tells the system exactly what kind of station this is
     @Override
     public String getStationType() {
         return "Satellite Station";
