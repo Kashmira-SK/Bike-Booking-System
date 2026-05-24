0a46c699ac8d0b4a41298e395249db8d01085899
YUNETHSUSARA <it25103730@my.sliit.lk>
2026-05-24 20:22:31 +0530

    Update BikeReview.java

--- src/main/java/com/bikerental/BikeRentalSystem/model/BikeReview.java
+++ src/main/java/com/bikerental/BikeRentalSystem/model/BikeReview.java
@@ -5,8 +5,10 @@ import com.bikerental.BikeRentalSystem.util.AppConstants;
 public class BikeReview extends Review {
 
     public BikeReview(String id, String userId, String targetId, int rating, String comment, String timestamp) {
+       // 'super' eken karanne parent class (Review) eke thiyena constructor ekata data pass karana eka.
         super(id, userId, targetId, rating, comment, AppConstants.REVIEW_BIKE, timestamp);
     }
+    // '@Override' kiyanne parent class eke liyala thibba bageyata liyapu method ekak, me child class eke athule sampurnayen liyanawa kiyana eka.
 
     @Override
     public String getReviewType() {
