62338cf7d955f422395a9d89da19fbf55e51af40
YUNETHSUSARA <it25103730@my.sliit.lk>
2026-05-24 20:39:58 +0530

    Update ServiceReview.java

--- src/main/java/com/bikerental/BikeRentalSystem/model/ServiceReview.java
+++ src/main/java/com/bikerental/BikeRentalSystem/model/ServiceReview.java
@@ -5,6 +5,7 @@ import com.bikerental.BikeRentalSystem.util.AppConstants;
 public class ServiceReview extends Review {
 
     public ServiceReview(String id, String userId, String targetId, int rating, String comment, String timestamp) {
+        // Data 7k illana nisa, uda thiyena 6th ekka type eka widihata 'AppConstants.REVIEW_SERVICE' kiyana eka denawa
         super(id, userId, targetId, rating, comment, AppConstants.REVIEW_SERVICE, timestamp);
     }
 
