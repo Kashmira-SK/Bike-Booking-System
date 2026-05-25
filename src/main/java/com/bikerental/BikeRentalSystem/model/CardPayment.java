389c141956f5368813f67c9d00750ad7939110fc
~N_I_L_A~ <it25103737@my.sliit.lk>
2026-05-25 21:00:01 +0530

    Update CardPayment.java

--- src/main/java/com/bikerental/BikeRentalSystem/model/CardPayment.java
+++ src/main/java/com/bikerental/BikeRentalSystem/model/CardPayment.java
@@ -3,11 +3,13 @@ package com.bikerental.BikeRentalSystem.model;
 import com.bikerental.BikeRentalSystem.util.AppConstants;
 
 public class CardPayment extends Payment {
-
+    
+     // Call parent constructor using super
     public CardPayment(String id, String rentalId, String userId, double amount, String status, String timestamp) {
         super(id, rentalId, userId, amount, AppConstants.METHOD_CARD, status, timestamp);
     }
 
+    // Override abstract method from Payment class
     @Override
      public String getPaymentMethod() {
         return "Card";
